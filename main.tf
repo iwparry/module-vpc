314b803ec6f771d605fe66e11973a8b45f9b58ca
iwparry <iwparry@hotmail.co.uk>
2024-04-21 17:56:02 +0100

    initial repo commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+# The VPC resource
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    "Name" = "vpc_module"
+  }
+}
+
+# Subnets
+resource "aws_subnet" "subnet" {
+    for_each = var.subnets
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+
+  tags = {
+    Name = "subnet-${each.key}"
+  }
+}
